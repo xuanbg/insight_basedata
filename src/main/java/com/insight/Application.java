a30607e9123a6b425e8a5d0a2ba9928f43ac8ff4
Brian <xan.brian@gmail.com>
2021-03-02 10:10:17 +0800

    up eureka to consul

--- src/main/java/com/insight/Application.java
+++ src/main/java/com/insight/Application.java
@@ -3,7 +3,7 @@ package com.insight;
 import com.insight.basedata.common.TaskRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Bean;
  * @remark 应用入口程序
  */
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableFeignClients
 public class Application {
 
